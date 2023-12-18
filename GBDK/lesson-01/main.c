ef84faeb4778e473a6f17782096f6c6af59a1069
James Couse <144252195+James-Couse@users.noreply.github.com>
2023-12-18 19:10:55 +0000

    setup complete

---
+++ GBDK/lesson-01/main.c
@@ -0,0 +1,13 @@
+// copyright (c) 2023 James Couse All rights reserved
+//
+// Created by: James Couse
+// Created on: Dec 2023
+// This program is the "Hello, World!" program for the GameBoy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main() {
+    // This prints "Hello, World!" to the GameBoy screen
+    printf("Hello, World!\n");
+}
